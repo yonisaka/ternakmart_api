cb59063d2fe2082ac48ad45637b407416457bcde
yonisaka <yonisaka0@gmail.com>
2021-05-02 17:59:12 +0700

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -73,7 +73,8 @@ $app->configure('app');
 */
 
 $app->middleware([
-    App\Http\Middleware\CorsMiddleware::class
+    // App\Http\Middleware\CorsMiddleware::class
+    \Fruitcake\Cors\HandleCors::class,
 ]);
 
 $app->routeMiddleware([
