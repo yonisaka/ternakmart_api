8f919d9a7045f5dcc4e82278323ca07706079c5e
yonisaka <yonisaka0@gmail.com>
2021-05-02 16:53:38 +0700

    update

--- app/Http/Controllers/TernakController.php
+++ app/Http/Controllers/TernakController.php
@@ -16,6 +16,9 @@ class TernakController extends Controller
     public function __construct()
     {
         //
+        Header('Access-Control-Allow-Origin: *'); //for allow any domain, insecure
+        Header('Access-Control-Allow-Headers: *'); //for allow any headers, insecure
+        Header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE'); //method allowed
     }
 
     public function index(){
