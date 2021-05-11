f41ffd1d5407b6054491c0f8a223cb1027e1f3f9
yonisaka <yonisaka0@gmail.com>
2021-05-11 11:16:44 +0700

    lokasi

--- app/Http/Controllers/LokasiController.php
+++ app/Http/Controllers/LokasiController.php
@@ -25,5 +25,22 @@ class LokasiController extends Controller
         return response()->json(['daftar_kota' =>  $data], 200);
     }
 
-    //
+    public function provinsi(){
+        $data = DB::table('lokasi')
+                ->select('province_id', 'province')
+                ->groupBy('province_id', 'province')
+                ->get();
+
+        return response()->json(['provinsi' =>  $data], 200);
+    }
+
+    public function kota($province_id){
+        $data = DB::table('lokasi')
+                ->select('city_id', 'type', 'city_name', 'postal_code')
+                ->where('province_id', $province_id)
+                ->get();
+
+        return response()->json(['kota' =>  $data], 200);
+    }
+    
 }
