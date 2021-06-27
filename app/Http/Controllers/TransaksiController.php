1d8ce9446b25c3a16caf1acf1bde14d1ab765e17
yonisaka <yonisaka0@gmail.com>
2021-06-27 19:43:03 +0700

    transaksi controller re pull

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Transaksi;
+use App\Models\Xendit;
+use App\Models\Invoices;
 use Illuminate\Support\Facades\DB;
 
 class TransaksiController extends Controller
@@ -66,10 +68,11 @@ class TransaksiController extends Controller
             $query = DB::table('transaksi')
                     ->leftJoin('ternak', 'transaksi.id_ternak', '=', 'ternak.id')
                     ->leftJoin('lokasi', 'transaksi.city_id', '=', 'lokasi.city_id')
-                    ->select('transaksi.*','ternak.ternak_nama','ternak.ternak_deskripsi', 
+                    ->leftJoin('jenis', 'ternak.id_jenis', '=', 'jenis.id')
+                    ->select('transaksi.*','ternak.ternak_nama','ternak.ternak_deskripsi', 'jenis.perawatan_harga', 
                     'ternak.file_path', 'lokasi.city_name', 'lokasi.province')
                     ->where('transaksi.id_user','=',$id)
-                    ->where('transaksi.transaksi_st','=', "cart");
+                    ->where('transaksi.transaksi_st','=', "CART");
             $cart = $query->get();
             $count = $query->count();
 
@@ -152,8 +155,10 @@ class TransaksiController extends Controller
         try {
             $query = DB::table('transaksi')
                     ->leftJoin('ternak', 'transaksi.id_ternak', '=', 'ternak.id')
-                    ->select('transaksi.*','ternak.ternak_nama','ternak.ternak_deskripsi', 
-                    'ternak.file_path')
+                    ->leftJoin('jenis', 'ternak.id_jenis', '=', 'jenis.id')
+                    ->leftJoin('users', 'users.id', '=', 'transaksi.id_user')
+                    ->select('transaksi.*','ternak.ternak_nama','ternak.ternak_deskripsi', 'jenis.perawatan_harga', 
+                    'ternak.file_path', 'users.email')
                     ->where('transaksi.id','=',$id);
                     // ->where('transaksi.transaksi_st','=', "cart");
             $cart = $query->first();
@@ -179,11 +184,13 @@ class TransaksiController extends Controller
     public function waiting($id){
         try {
             $query = DB::table('transaksi')
-                    ->leftJoin('ternak', 'transaksi.id_ternak', '=', 'ternak.id')
-                    ->select('transaksi.*','ternak.ternak_nama','ternak.ternak_deskripsi', 
-                    'ternak.file_path')
-                    ->where('transaksi.id_user','=',$id)
-                    ->where('transaksi.transaksi_st','=', "waiting_payment");
+                ->leftJoin('ternak', 'transaksi.id_ternak', '=', 'ternak.id')
+                ->leftJoin('lokasi', 'transaksi.city_id', '=', 'lokasi.city_id')
+                ->leftJoin('jenis', 'ternak.id_jenis', '=', 'jenis.id')
+                ->select('transaksi.*','ternak.ternak_nama','ternak.ternak_deskripsi', 'jenis.perawatan_harga', 
+                'ternak.file_path', 'lokasi.city_name', 'lokasi.province')
+                ->where('transaksi.id_user','=',$id)
+                ->where('transaksi.transaksi_st','=', "PENDING");
             $cart = $query->get();
             $count = $query->count();
 
@@ -201,7 +208,7 @@ class TransaksiController extends Controller
                     ->select('transaksi.*','ternak.ternak_nama','ternak.ternak_deskripsi', 
                     'ternak.file_path')
                     ->where('transaksi.id_user','=',$id)
-                    ->where('transaksi.transaksi_st','=', "confirmation");
+                    ->where('transaksi.transaksi_st','=', "PAID");
             $cart = $query->get();
             $count = $query->count();
 
@@ -213,61 +220,148 @@ class TransaksiController extends Controller
         }
     }
 
-    public function getToken(Request $request){
-        // Set your Merchant Server Key
-        \Midtrans\Config::$serverKey = 'SB-Mid-server-EVovFI-eT7n5eLm6J_VaFKyw';
-        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
-        \Midtrans\Config::$isProduction = false;
-        // Set sanitization on (default)
-        \Midtrans\Config::$isSanitized = true;
-        // Set 3DS transaction for credit card to true
-        \Midtrans\Config::$is3ds = true;
+    // public function getToken(Request $request){
+    //     // Set your Merchant Server Key
+    //     \Midtrans\Config::$serverKey = 'SB-Mid-server-EVovFI-eT7n5eLm6J_VaFKyw';
+    //     // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+    //     \Midtrans\Config::$isProduction = false;
+    //     // Set sanitization on (default)
+    //     \Midtrans\Config::$isSanitized = true;
+    //     // Set 3DS transaction for credit card to true
+    //     \Midtrans\Config::$is3ds = true;
         
-        $params = array(
-            'transaction_details' => array(
-                'order_id' => $request->input('order_id'),
-                'gross_amount' => $request->input('gross_amount'),
-            ),
-            'customer_details' => array(
-                'first_name' => $request->input('name'),
-                'last_name' => $request->input('last_name'),
-                'email' => $request->input('email'),
-                'phone' => $request->input('phone'),
-            ),
+    //     $params = array(
+    //         'transaction_details' => array(
+    //             'order_id' => $request->input('order_id'),
+    //             'gross_amount' => $request->input('gross_amount'),
+    //         ),
+    //         'customer_details' => array(
+    //             'first_name' => $request->input('name'),
+    //             'last_name' => $request->input('last_name'),
+    //             'email' => $request->input('email'),
+    //             'phone' => $request->input('phone'),
+    //         ),
+    //     );
+
+    //     $query = DB::table('transaksi')
+    //                 ->select('transaksi_token')
+    //                 ->where('order_id','=',$request->input('order_id'));
+    //     $isTokenize = $query->first();
+
+    //     if($isTokenize->transaksi_token == null){
+    //         $snapToken = \Midtrans\Snap::getSnapToken($params);
+    //         $affected = DB::table('transaksi')
+    //           ->where('order_id', $request->input('order_id'))
+    //           ->update(['transaksi_token' => $snapToken]);
+    //         return response()->json(['token' => $snapToken], 200);
+    //     }
+    //     else{
+    //         $query = DB::table('transaksi')
+    //         ->select('transaksi_token')
+    //         ->where('order_id','=',$request->input('order_id'));
+    //         $token = $query->first();
+
+    //         return response()->json(['token' => $token->transaksi_token ],  200);
+    //     }
+            
+    // }
+
+    // public function updateToken(Request $request){
+    //     $affected = DB::table('transaksi')
+    //           ->where('id', $request->input('id'))
+    //           ->update(['transaksi_token' => $request->input('token')]);
+    //     if($affected){
+    //         return response()->json(['message' => 'Berhasil Update Data'], 200);
+    //     }
+        
+    // }
+
+    public function createInvoice(Request $request){
+
+        $data = array(
+            'external_id' => $request->input('external_id'),
+            'amount' => $request->input('amount'),
+            'payer_email' => $request->input('payer_email'),
+            'description' => $request->input('description'),
         );
 
-        $query = DB::table('transaksi')
-                    ->select('transaksi_token')
-                    ->where('order_id','=',$request->input('order_id'));
-        $isTokenize = $query->first();
+        $transaksi = DB::table('transaksi')
+            // ->join('invoices', 'transaksi.invoice', '=', 'invoices.invoice')
+            ->select('transaksi.*')
+            ->where('transaksi.order_id', $request->input('order_id'))
+            ->get();
 
-        if($isTokenize->transaksi_token == null){
-            $snapToken = \Midtrans\Snap::getSnapToken($params);
+        if($transaksi[0]->invoice ==null || $transaksi[0]->transaksi_st == "EXPIRED"){
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.xendit.co/v2/invoices',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS =>json_encode($data),
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Authorization: Basic eG5kX2RldmVsb3BtZW50X3dhdzlucmJ0TlNRQk5VVmJvNDdoUXdrVXdQcWRNNTVkQ0lWM0RORk5lVFBEa2w1Sndad2VST25RYWE0aW1qZUY6',
+                'Cookie: visid_incap_2182539=f5doSCL4TcW2shUF74hn0cq5wWAAAAAAQUIPAAAAAAAdtoEduPjphkSZEy6WRCyn'
+            ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+
+                //log
+            $data = new Xendit;
+            $data->log = $response;
+            $data->save();
+
+            $array = json_decode($response, true);
+
             $affected = DB::table('transaksi')
-              ->where('order_id', $request->input('order_id'))
-              ->update(['transaksi_token' => $snapToken]);
-            return response()->json(['token' => $snapToken], 200);
-        }
-        else{
-            $query = DB::table('transaksi')
-            ->select('transaksi_token')
-            ->where('order_id','=',$request->input('order_id'));
-            $token = $query->first();
+                ->where('order_id', $request->input('order_id'))
+                ->update(['invoice' => $array['external_id'],
+                        'transaksi_st' => $array['status']]);
 
-            return response()->json(['token' => $token->transaksi_token ],  200);
-        }
-            
-    }
+            //Add to incoices table
+            $invoice = new Invoices;
+            $invoice->id = $array['id'];
+            $invoice->order_id = $request->input('order_id');
+            $invoice->invoice = $array['external_id'];
+            $invoice->status = $array['status'];
+            $invoice->amount = $array['amount'];
+            $invoice->email = $array['payer_email'];
+            $invoice->description = $array['description'];
+            $invoice->expiry_date = $array['expiry_date'];
+            $invoice->invoice_url = $array['invoice_url'];
+            $invoice->save();
 
-    public function updateToken(Request $request){
-        $affected = DB::table('transaksi')
-              ->where('id', $request->input('id'))
-              ->update(['transaksi_token' => $request->input('token')]);
-        if($affected){
-            return response()->json(['message' => 'Berhasil Update Data'], 200);
+            return response()->json(['Response' => json_decode($response)], 200);
         }
         
+        else if($transaksi[0]->transaksi_st == "PENDING"){
+            $transaksiInvoice = DB::table('transaksi')
+            ->join('invoices', 'transaksi.invoice', '=', 'invoices.invoice')
+            ->select('transaksi.*', 'invoices.*')
+            ->where('transaksi.order_id', $request->input('order_id'))
+            ->get();
+            $url['invoice_url'] = $transaksiInvoice[0]->invoice_url;
+            return response()->json(['Response' => $url], 200);
+        }
+        else{
+            $transaksiInvoice = DB::table('transaksi')
+            ->join('invoices', 'transaksi.invoice', '=', 'invoices.invoice')
+            ->select('transaksi.*', 'invoices.*')
+            ->where('transaksi.order_id', $request->input('order_id'))
+            ->get();
+            $url['invoice_url'] = $transaksiInvoice[0]->invoice_url;
+            return response()->json(['Response' => $url], 200);
+        }
+        // return response()->json(['Response' => "hehe"], 200);
+        // return response()->json(['Response' => json_decode($response)], 200);
     }
-
-    //
 }
