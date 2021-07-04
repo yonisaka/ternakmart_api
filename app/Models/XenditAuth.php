89210ebbb75eee84cdf18e47a5a1ec2a699f13f1
yonisaka <yonisaka0@gmail.com>
2021-07-04 14:04:47 +0700

    send_mail route

--- app/Models/XenditAuth.php
+++ app/Models/XenditAuth.php
@@ -9,6 +9,6 @@ class XenditAuth extends Model
     public static function basic_auth() {
         $dev = 'Basic eG5kX2RldmVsb3BtZW50X3dhdzlucmJ0TlNRQk5VVmJvNDdoUXdrVXdQcWRNNTVkQ0lWM0RORk5lVFBEa2w1Sndad2VST25RYWE0aW1qZUY6';
         $prod = 'Basic eG5kX3Byb2R1Y3Rpb25fTHB6bE1BM2dDQXBrN242M2JJT2xpTTVVeGVuZEV4YUkwZ2VZSUZ0ZVdJa0o3eEx1RUljczdFWUE5VDVBZ0I6';
-        return $prod;
+        return $dev;
     } 
 }
